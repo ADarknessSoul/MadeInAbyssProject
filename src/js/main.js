77d32c1b6e9f10aabf8af1a9f4e1bd38e11dccbf
Gerardo <a21110179@ceti.mx>
2022-08-26 02:47:50 -0500

    Acabé la sección desplegable con la interacción de JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
   
-    abrirMapa();
+    mapa();
 
 })
+
+const mapa = () => {
+
+    abrirMapa();
+    cambiarImagen();
+
+}
 
 const abrirMapa = () => {
 
@@ -22,5 +29,101 @@ const abrirMapa = () => {
         desplegable.classList.remove('mapa');
 
     });
+
+}
+
+const cambiarImagen = () => {
+
+    const scroll__izq = document.querySelector('.mapa__img1');
+    const scroll__der = document.querySelector('.mapa__img3');
+    const max = 7;
+    const min = 1;
+    const maxPortada = 14;
+    const minPortada = 8;
+
+    scroll__der.addEventListener('click', () => {
+
+        let imagen = document.querySelector('.mapa__img2');
+        let portada = document.querySelector('.mapa__portada');
+        let newIdPortada = parseInt(portada.id) + 1;
+        let newId = parseInt(imagen.id) + 1;
+
+        if(newIdPortada <= maxPortada) {
+            newIdPortada = newIdPortada + '';
+            portada.remove();
+            crearPortada(newIdPortada);
+        }
+
+        if(newId <= max) {
+
+            newId = newId + '';
+            imagen.remove();
+            cambiarH1(newId);
+            crearImagen(newId);
+        }
+
+    });
+
+    scroll__izq.addEventListener('click', () => {
+
+        let imagen = document.querySelector('.mapa__img2');
+        let newId = parseInt(imagen.id) - 1;
+        let portada = document.querySelector('.mapa__portada');
+        let newIdPortada = parseInt(portada.id) - 1;
+        
+        if(newIdPortada >= minPortada) {
+            newIdPortada = newIdPortada + '';
+            portada.remove();
+            crearPortada(newIdPortada);
+        }
+
+        if(newId >= min) {
+
+            newId = newId + '';
+            imagen.remove();
+            cambiarH1(newId);
+            crearImagen(newId);
+
+        }
+
+    });
+
+
+
+}
+
+const crearImagen = (newId) => {
+
+    const newImagen = document.createElement('img');
+    const div = document.querySelector('#imagenAbismo');
+
+    newImagen.setAttribute("id", newId);
+    newImagen.classList.add('mapa__img2');
+    newImagen.src = "../../images/Maldiciones/Maldicion" + newId + ".jpg";
+    
+
+    div.appendChild(newImagen);
+
+}
+
+const crearPortada = (newId) => {
+
+    const newImagen = document.createElement('img');
+    const div = document.querySelector('#mapaAbismo');
+
+    newImagen.setAttribute("id", newId);
+    newImagen.classList.add('mapa__portada');
+    newImagen.src = "../../images/Capas/Capa" + newId + ".jpg";
+    
+
+    div.appendChild(newImagen);
+
+}
+
+const cambiarH1 = (Capa) => {
+
+    const H1 = document.getElementById('numeroCapa');
+    H1.innerHTML = `Capa ${Capa}`
+
 
 }
