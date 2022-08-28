5b330c280f8588176777fe2bd935aa45b09f9f19
Gerardo <a21110179@ceti.mx>
2022-08-27 20:54:11 -0500

    Acabe todas las funcionalidades de la página, contando la maximización de la imagen del mapa, el desplazamiento hasta la capa elegida y los audios en cada article

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,8 @@ const mapa = () => {
 
     abrirMapa();
     cambiarImagen();
+    desplazarCapa();
+    reproducirAudio();
 
 }
 
@@ -40,6 +42,7 @@ const cambiarImagen = () => {
     const min = 1;
     const maxPortada = 14;
     const minPortada = 8;
+    let mapaCompleto = document.querySelector('.mapa__contenedor');
 
     scroll__der.addEventListener('click', () => {
 
@@ -88,6 +91,12 @@ const cambiarImagen = () => {
 
     });
 
+    mapaCompleto.addEventListener('click', () => {
+
+        mostrar();
+
+    });
+
 
 
 }
@@ -125,5 +134,170 @@ const cambiarH1 = (Capa) => {
     const H1 = document.getElementById('numeroCapa');
     H1.innerHTML = `Capa ${Capa}`
 
+
+}
+
+const desplazarCapa = () => {
+
+    const frame = document.getElementById('imagenAbismo');
+    const desplegable = document.querySelector('#mapa');
+
+    frame.addEventListener('click', (e) => {
+
+        let Image = document.querySelector('.mapa__img2');
+        e.preventDefault(); //Se previene la acción por default para que no nos lleve de golpe al id
+
+        const seccion = document.getElementById('Capa' + Image.id);
+        desplegable.classList.remove('mapa');
+        seccion.scrollIntoView({behavior: "smooth"}); //Objeto de configuración
+
+    });
+
+
+}
+
+
+
+const mostrar = () => {
+
+    let img = document.createElement('IMG');
+    let mapa = document.querySelector('.mapa__portada');
+
+    console.log('../../images/Capas/Capa' + mapa.id + '.jpg');
+
+    img.src = '../../images/Capas/Capa' + mapa.id + '.jpg';
+
+    //Crear overlay con imágen
+    const overlay = document.createElement('DIV');
+    overlay.appendChild(img);
+    overlay.classList.add('overlay');
+    overlay.onclick = function() {
+        overlay.remove(); //Método de javascript para eliminar el elemento
+        img.remove();
+    }
+
+    const body = document.querySelector('body');
+    body.appendChild(overlay);
+
+}
+
+const reproducirAudio = () => {
+
+    const firstLayer = document.getElementById('Capa1');
+    const secondLayer = document.getElementById('Capa2');
+    const thirdLayer = document.getElementById('Capa3');
+    const fourthLayer = document.getElementById('Capa4');
+    const fifthLayer = document.getElementById('Capa5');
+    const sixthLayer = document.getElementById('Capa6');
+    const seventhLayer = document.getElementById('Capa7');
+
+    const audio1 = new Audio('../../sounds/The_First_Layer.mp3');
+    const audio2 = new Audio('../../sounds/Made_in_Abyss.mp3');
+    const audio3 = new Audio('../../sounds/To_the_Abyss.mp3');
+    const audio4 = new Audio('../../sounds/The_Fourth_Layer.mp3');
+    const audio5 = new Audio('../../sounds/Dawn_of_the_deep_soul.mp3');
+    const audio6 = new Audio('../../sounds/Made_in_Abyss_Op_2.mp3');
+    const audio7 = new Audio('../../sounds/Made_in_Abyss_Ending_2.mp3');
+
+    const arreglo = [firstLayer, secondLayer, thirdLayer, fourthLayer, fifthLayer, sixthLayer, seventhLayer];
+
+    arreglo.forEach(Capa => {
+        Capa.addEventListener('mouseenter', event => {
+            
+            switch(event.target.id) {
+                case 'Capa1': 
+                    desmutear(audio1);   
+                break;
+
+                case 'Capa2':
+                    desmutear(audio2);
+                break;
+
+                case 'Capa3':
+                    desmutear(audio3);
+                break;
+
+                case 'Capa4':
+                    desmutear(audio4);
+                break;
+
+                case 'Capa5':
+                    desmutear(audio5);
+                break;
+
+                case 'Capa6':
+                    desmutear(audio6);
+                break;
+
+                case 'Capa7':
+                    desmutear(audio7);
+                break;
+
+                default:
+
+                console.error('No se pudo encontrar ningún audio');
+
+                break;
+
+            }
+
+        });
+    });
+
+    arreglo.forEach(Capa => {
+        Capa.addEventListener('mouseleave', event => {
+            
+            switch(event.target.id) {
+                case 'Capa1': 
+                    mutear(audio1);   
+                break;
+
+                case 'Capa2':
+                    mutear(audio2);
+                break;
+
+                case 'Capa3':
+                    mutear(audio3);
+                break;
+
+                case 'Capa4':
+                    mutear(audio4);
+                break;
+
+                case 'Capa5':
+                    mutear(audio5);
+                break;
+
+                case 'Capa6':
+                    mutear(audio6);
+                break;
+
+                case 'Capa7':
+                    mutear(audio7);
+                break;
+
+                default:
+
+                console.error('No se pudo encontrar ningún audio');
+
+                break;
+            }
+
+        });
+    });
+
+}
+
+const desmutear = (audio) => {
+    
+    console.log(audio);
+
+    audio.play();
+
+}
+
+const mutear = (audio) => {
+
+    audio.pause();
 
 }
